92478c2d95932da4fd5b57fddd947311a36bb287
Jishnu-k-b <jishnu.kb17291@gmail.com>
2023-05-03 12:23:42 +0530

    home_fix

--- faculty_app/urls.py
+++ faculty_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('home', views.home, name='home'),
+    path('', views.home, name='home'),
+    path('index/', views.index, name='index'),
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
